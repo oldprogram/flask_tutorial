b995890edd319c18f00537791340fd438c4a4ef6
litao <litao@tuya.com>
2024-02-19 17:19:38 +0800

    1. 完善图床，参考 VERSION.md

---
+++ VERSION.md
@@ -0,0 +1,26 @@
+## V2.0
+
+实现 http://127.0.0.1:5000/tuchuang 
+
+- 一个 bakend 后台页面（找一个 moban6764.rar 的 Html 模板）
+- 编写一个支持翻页的图片浏览页面（list，带翻页）（涉及大量 jinja 知识）
+- 编写一个支持批量图片上传的页面（涉及一个典型的 js 知识）
+
+注意：python 版本需要大于等于 3.9
+
+</br>
+
+upload 页面：
+
+![][p1]
+
+</br>
+
+list 页面：
+
+![][p2]
+
+
+
+[p1]:./doc/pic/tuchuang_upload.png
+[p2]:./doc/pic/tuchuang_list.png
--- flaskr/tuchuang.py
+++ flaskr/tuchuang.py
@@ -3,6 +3,8 @@
 
 import os
 import random
+import time
+import datetime
 from datetime import datetime
 from flask import Blueprint
 from flask import Flask, flash, request, redirect, url_for
@@ -24,11 +26,34 @@ from flaskr.db import get_db
 bp = Blueprint("tuchuang", __name__, url_prefix="/tuchuang")
 
 
+def search(path):
+    file_list = []
+    prefix = path
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_size = os.path.getsize(file_path)
+            file_time = time.strftime("%Y-%m-%d %H:%M:%S",
+                    time.localtime(os.path.getmtime(file_path)))
+            img = Img(file_path.removeprefix(prefix),file,file_size,["A","B"],file_time)  
+            file_list.append(img)
+    file_list.sort(key=lambda x: x.date, reverse=True)
+
+    return file_list
+
+class Img:
+    def __init__(self, path, name, size, kind, date):
+        self.path = path
+        self.name = name
+        self.size = size
+        self.kind = kind
+        self.date = date
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/upload', methods=['GET', 'POST'])
 @login_required
 def upload_file():
     if request.method == 'POST':
@@ -49,7 +74,8 @@ def upload_file():
             # 生成随机数
             random_num = random.randint(0, 100)
             # f.filename.rsplit('.', 1)[1] 获取文件的后缀
-            filename = datetime.now().strftime("%Y%m%d%H%M%S") + "_" + str(random_num) + "." + filename.rsplit('.', 1)[1]
+            # filename = datetime.now().strftime("%Y%m%d%H%M%S") + "_" + str(random_num) + "." + filename.rsplit('.', 1)[1]
+            filename = datetime.now().strftime("%Y%m%d%H%M%S") + "_" + filename
             file_path = app.config['UPLOAD_FOLDER']    # basedir 代表获取当前位置的绝对路径
             
             # 如果文件夹不存在，就创建文件夹
@@ -64,4 +90,27 @@ def upload_file():
 def download_file(name):
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
     
-    
+@bp.route('/view/page=<page>')
+def view(page):
+    print(os.getcwd()) # 输出当前工作目录的路径
+    print(os.path.abspath('.')) # 输出当前文件所在目录的绝对路径
+
+    imgs = search("./flaskr/static/file/img",)
+    total_num = len(imgs)
+    step = 10
+    index_start = int(page) * step
+    index_end = int(page) * step + step
+    if index_end > total_num:
+        index_end = total_num
+
+    return render_template('tuchuang/view.html', 
+            img_start = index_start, 
+            img_end = index_end,
+            img_step = step,
+            img_num = total_num ,
+            my_imgs=imgs[index_start:index_end])
+
+@bp.route('/')
+def main():
+    return render_template('tuchuang/backend.html')
+   

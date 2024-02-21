fe887087add0d6705abed22220e0d44bcda5cafb
litao <litao@tuya.com>
2024-02-21 21:27:08 +0800

    1.新增 share 页面，支持将文件上传、下载、删除，利用 css,js 主要是 js 技术

---
+++ flaskr/static/file/js/tuchuang/share.js
@@ -0,0 +1,147 @@
+window.onload=function(){
+    var dropArea = document.getElementById('drop-area')
+
+    // 阻止默认行为
+    ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, preventDefaults, false)
+    })
+
+    function preventDefaults (e) {
+        e.preventDefault()
+        e.stopPropagation()
+    }
+
+    // 增加事件，鼠标拖入边框高亮，拖出边框变为原来样子
+    ;['dragenter', 'dragover'].forEach(eventName => {
+        dropArea.addEventListener(eventName, highlight, false)
+    })
+
+    ;['dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, unhighlight, false)
+    })
+
+    function highlight(e) {
+        dropArea.classList.add('highlight')
+    }
+
+    function unhighlight(e) {
+        dropArea.classList.remove('highlight')
+    }
+
+    // 增加事件，鼠标放下，之后准备上传图片
+    dropArea.addEventListener('drop', handleDrop, false)
+
+    function handleDrop(e) {
+        let dt = e.dataTransfer
+        let files = dt.files
+
+        handleFiles(files)
+    }
+}
+
+function uploadFile(file) {
+    let url = '/tuchuang/share'
+    let formData = new FormData()
+
+    formData.append('file', file)
+
+    fetch(url, {
+        method: 'POST',
+        body: formData
+    })
+    .then(function (response) {
+        //JavaScript fetch API - 为什么response.json()返回一个promise对象（而不是JSON）
+        //https://stackoverflow.com/questions/39435842/javascript-fetch-api-why-does-response-json-return-a-promise-object-instead
+        //在 JavaScript 中使用 fetch() 时如何获取响应正文
+        //https://byby.dev/js-fetch-get-response-body
+        //w3s table
+        //https://www.w3schools.com/html/tryit.asp?filename=tryhtml_table_intro
+        //https://www.w3schools.com/html/html_tables.asp
+        //w3s JS table
+        //https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_table_create
+        response.json().then(function(json){
+            if(json.state == 'success'){
+                //console.log(json); // 打印获取到的数据
+                console.log(JSON.stringify(json))
+        
+                let tr = document.createElement('tr')                
+                let td_name = document.createElement('td')
+                td_name.innerText = json.name
+                let td_size = document.createElement('td')
+                td_size.innerText = json.size
+                let td_kind = document.createElement('td')
+                td_kind.innerText = json.kind
+                let td_date = document.createElement('td')
+                td_date.innerText = json.date
+                
+                // delete and download button
+                let td_action = document.createElement('td')
+                let action_container = document.createElement('div')
+                action_container.setAttribute("class", "button-container");
+
+                let btn_delete = document.createElement('button')
+                btn_delete.setAttribute("class","flex items-center justify-between px-0 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray")
+                btn_delete.setAttribute("id", "btn_delete");
+                btn_delete.setAttribute("onclick", "deleteFile(this)");
+                btn_delete.innerText = 'delete'
+                action_container.appendChild(btn_delete)
+
+                let btn_download = document.createElement('button')
+                btn_download.setAttribute("class","flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray")
+                btn_download.setAttribute("id", "btn_download");
+                btn_download.setAttribute("onclick", "downloadFile(\""+json.name+"\")");
+                btn_download.innerText = 'download'
+                action_container.appendChild(btn_download)
+                
+                td_action.appendChild(action_container)
+                
+                //a.setAttribute("href", "/tuchuang/download_share/"+json.name);
+
+                tr.appendChild(td_name)
+                tr.appendChild(td_size)
+                tr.appendChild(td_kind)
+                tr.appendChild(td_date)
+                tr.appendChild(td_action)
+                document.getElementById('gallery').appendChild(tr)
+            }
+        });
+    })
+    .catch(() => { /* Error. Inform the user */ })
+}
+
+function handleFiles(files) {
+    files = [...files]
+    files.forEach(uploadFile)
+    
+    //input输入框file类型第二次不触发onchange事件
+    //https://blog.csdn.net/jianghaha2011/article/details/121821387
+    document.getElementById('fileElem').value = ''
+}
+
+function deleteFile(btn) {
+    var row = btn.parentNode.parentNode.parentNode; // 获取按钮所在的行
+    var file_name = row.cells[0].innerText; // 获取第一个单元格的内容
+
+    let url = '/tuchuang/delete_share/'+file_name
+    fetch(url) // 指定要获取数据的 URL
+    .then(response => response.json()) // 将返回的响应转换为 JSON 格式
+        .then(data => {
+            console.log(JSON.stringify(data)); // 打印从服务器接收到的数据
+        })
+    .catch(error => {
+        console.error('Error:', error); // 处理错误情况
+    });
+
+    row.parentNode.removeChild(row); // 从 DOM 中移除行
+    console.log(file_name)
+}
+
+function downloadFile(fileName) {
+    let url = '/tuchuang/download_share/' + fileName;
+    var link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
--- flaskr/tuchuang.py
+++ flaskr/tuchuang.py
@@ -7,52 +7,64 @@ import time
 import datetime
 from datetime import datetime
 from flask import Blueprint
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, jsonify
 from flask import send_from_directory
 from flask import render_template
 from werkzeug.utils import secure_filename
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 UPLOAD_FOLDER = basedir+'/static/file/img'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+SHARE_FOLDER = basedir+'/static/file/share'
+ALLOWED_IMG_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_SHARE_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip', 'rar', 'py', 'c'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+app.config['SHARE_FOLDER'] = SHARE_FOLDER
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint("tuchuang", __name__, url_prefix="/tuchuang")
 
-
-def search(path):
-    file_list = []
-    prefix = path
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            file_size = os.path.getsize(file_path)
-            file_time = time.strftime("%Y-%m-%d %H:%M:%S",
+#####################################################################################
+class File:
+    def __init__(self, file_path,prefix):
+        self.path = file_path
+        self.url  = self.path.removeprefix(prefix)
+        self.name = os.path.basename(file_path)
+        self.size = os.path.getsize(file_path)
+        self.kind = self.name.rsplit('.', 1)[1].lower() 
+        self.date = time.strftime("%Y-%m-%d %H:%M:%S",
                     time.localtime(os.path.getmtime(file_path)))
-            img = Img(file_path.removeprefix(prefix),file,file_size,["A","B"],file_time)  
-            file_list.append(img)
-    file_list.sort(key=lambda x: x.date, reverse=True)
 
-    return file_list
+    def info_to_json(self):
+        return {'name':self.name,'size':self.size,'kind':self.kind,'date':self.date, 'url':self.url}
+    
+    @staticmethod
+    def search(path):
+        file_list = []
+        prefix = path
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                img = File(file_path,prefix)
+                file_list.append(img)
+        file_list.sort(key=lambda x: x.date, reverse=True)
 
-class Img:
-    def __init__(self, path, name, size, kind, date):
-        self.path = path
-        self.name = name
-        self.size = size
-        self.kind = kind
-        self.date = date
+        return file_list
+    
+    @staticmethod
+    def allowed_file(filename,kind):
+        if kind == 'img':
+            return '.' in filename and \
+                filename.rsplit('.', 1)[1].lower() in ALLOWED_IMG_EXTENSIONS
+        elif kind == 'share':
+            return '.' in filename and \
+                filename.rsplit('.', 1)[1].lower() in ALLOWED_SHARE_EXTENSIONS
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+#####################################################################################
 @bp.route('/upload', methods=['GET', 'POST'])
 @login_required
 def upload_file():
@@ -67,7 +79,7 @@ def upload_file():
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        if file and allowed_file(file.filename):
+        if file and File.allowed_file(file.filename,'img'):
             # 获取安全的文件名 正常文件名
             filename = secure_filename(file.filename)
             
@@ -86,16 +98,72 @@ def upload_file():
             return redirect(url_for('tuchuang.download_file', name=filename))
     return render_template("tuchuang/upload.html")
     
+@bp.route('/share', methods=['GET', 'POST'])
+@login_required
+def share_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and File.allowed_file(file.filename,'share'):
+            # 获取安全的文件名 正常文件名
+            filename = secure_filename(file.filename)
+            
+            # 生成随机数
+            random_num = random.randint(0, 100)
+            # f.filename.rsplit('.', 1)[1] 获取文件的后缀
+            # filename = datetime.now().strftime("%Y%m%d%H%M%S") + "_" + str(random_num) + "." + filename.rsplit('.', 1)[1]
+            filename = datetime.now().strftime("%Y%m%d%H%M%S") + "_" + filename
+            file_path = app.config['SHARE_FOLDER']    # basedir 代表获取当前位置的绝对路径
+            
+            # 如果文件夹不存在，就创建文件夹
+            if not os.path.exists(file_path):
+	            os.makedirs(file_path)
+	
+            file.save(os.path.join(file_path, filename))
+            
+            file_info = File(os.path.join(file_path, filename),file_path).info_to_json()
+            file_info['state'] = 'success'
+ 
+            return jsonify(file_info)
+        else:
+            return jsonify(state="fail",reason="file kind not allowed")
+        
+    return render_template("tuchuang/share.html")
+
 @bp.route('/download/<name>')
 def download_file(name):
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
     
+@bp.route('/download_share/<name>')
+def download_share_file(name):
+    return send_from_directory(app.config["SHARE_FOLDER"], name)
+
+@bp.route('/delete_share/<name>')
+@login_required
+def delete_share_file(name):
+    # https://blog.csdn.net/weixin_43215588/article/details/121189959
+    # Flask——返回json数据的方法
+    name = os.path.basename(name)
+    file = app.config["SHARE_FOLDER"]+'/'+name
+    if os.path.exists(file):
+        os.remove(file)
+    
+    return jsonify(state="success")
+
 @bp.route('/view/page=<page>')
 def view(page):
     print(os.getcwd()) # 输出当前工作目录的路径
     print(os.path.abspath('.')) # 输出当前文件所在目录的绝对路径
 
-    imgs = search("./flaskr/static/file/img",)
+    imgs = File.search("./flaskr/static/file/img")
     total_num = len(imgs)
     step = 10
     index_start = int(page) * step

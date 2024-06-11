667109a30c17a9841ffe29f7d781a0596e8bcbd4
litao <litao@tuya.com>
2024-06-11 20:23:23 +0800

    1.微调

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+#python3.9 -m venv venv
+#pip install flask
+#pip install python_avatars
+#pip install pypinyin
 . ./venv/bin/activate
 
 export FLASK_APP=flaskr
 export FLASK_ENV=development
+#flask init-db
 flask run --host=0.0.0.0 --port=5000
